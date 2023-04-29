e2e251d98fb13fa7a1a6fcacc53393a55cf54ef1
sunilmondal4you <sunilmondal4you@gmail.com>
2023-04-29 11:04:42 +0530

    express statement

--- src/main.js
+++ src/main.js
@@ -3,8 +3,8 @@ import { saveOneDocument } from "./appmongo/insert.util.js";
 import { readDocuments } from "./appmongo/read.util.js";
 
 const app = express();
-app.use(express.json()); // for parsing application/json
-app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", async (req, res) => {
   let list = await readDocuments();
